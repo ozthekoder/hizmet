3bf889a9e12623ba924488ad773c4e9b8c2973c6
ozthekoder <oozdemir2704@gmail.com>
2014-04-13 14:24:24 -0400

    applicants page added

--- modules/applicants/templates/ApplicantRow.view.php
+++ modules/applicants/templates/ApplicantRow.view.php
@@ -5,7 +5,7 @@
     <td name="firstName" editable="true"><span><?= $firstName ?></span></td>
     <td name="lastName" editable="true"><span><?= $lastName ?></span></td>
     <td name="dob" editable="true"><span><?= $dob ?></span></td>
-    <td name="gender" editable="true"><span><?= $id == 0 ? 'Male' : 'Female' ?></span></td>
+    <td name="gender" editable="true"><span><?= $id == 1 ? 'Male' : 'Female' ?></span></td>
     <td name="avatar" editable="false"><span><img src="<?= EventManager::url($avatar) ?>" width="24" height="24"/></span></td>
     <td name="accountType" editable="true"><span><?= $id == REGULAR ? 'Applicant' : 'Admin' ?></span></td>
     <td name="resume" editable="false"><span><?= $resume ?></span></td>
