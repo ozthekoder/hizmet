9c9cd9ab978f725e3e15635ed29b67f8220ca46a
ozthekoder <oozdemir2704@gmail.com>
2014-08-17 19:20:19 -0400

    asd

--- modules/admin/templates/LeftPanel.view.php
+++ modules/admin/templates/LeftPanel.view.php
@@ -3,7 +3,7 @@
         <li><a <?php if(EventManager::$currentModule == 'Federations') echo 'class="selected-nav-item"'; ?> href="<?= EventManager::url('admin/federations') ?>"><span class="icon-building" style="margin-right:25px;"></span>Federations</a></li>
         <li><a <?php if(EventManager::$currentModule == 'Regions') echo 'class="selected-nav-item"'; ?> href="<?= EventManager::url('admin/regions') ?>"><span class="icon-us-continental" style="margin-right:25px;"></span>Regions</a></li>
       <li><a <?php if(EventManager::$currentModule == 'Applicants') echo 'class="selected-nav-item"'; ?> href="<?= EventManager::url('admin/applicants') ?>"><span class="icon-users" style="margin-right:25px;"></span>Applicants</a></li>
-      <?php if($_SESSION['user']->accountType == ADMIN): ?>
+      <?php if($_SESSION['user']->accountType > REGULAR): ?>
       <li><a  <?php if(EventManager::$currentModule == 'Moderators') echo 'class="selected-nav-item"'; ?> href="<?= EventManager::url('admin/moderators') ?>"><span class="icon-users-1" style="margin-right:25px;"></span>Moderators</a></li>
       <?php endif; ?>
       <li><a  <?php if(EventManager::$currentModule == 'Applications') echo 'class="selected-nav-item"'; ?> href="<?= EventManager::url('admin/applications') ?>"><span class="icon-file" style="margin-right:25px;"></span>Applications</a></li>
